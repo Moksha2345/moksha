61ea3c74395f56c67430cbda48f16b720dd535c9
Moksha2345 <mokshamoksha726@gmail.com>
2024-05-15 15:44:52 +0530

    mk

---
+++ Script.js
@@ -0,0 +1,11 @@
+function validateage(){
+    var name = document.getElementById("name");
+    var age =  document.getElementById("age").value;
+    var result=document.querySelector("p";)
+
+    if(age>=18){
+        result.innerHTML="you are eligible to vote";
+}else{
+        result.innerHTML="you are still child😂";
+}
+}
